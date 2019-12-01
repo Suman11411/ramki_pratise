53a0743e44624be351e03fd86bedee38bd15b2ce
RAGHU <raghuss11411@gmail.com>
2019-12-01 18:52:46 +0530

    added and commited in one line

--- credit.java
+++ credit.java
@@ -1,1 +1,6 @@
 credit
+1.
+the ability of a customer to obtain goods or services before payment, based on the trust that payment will be made in the future.
+"I've got unlimited credit"
+synonyms:	financial standing, financial status, solvency
+"the shop would be paid whether her credit was good or bad"
